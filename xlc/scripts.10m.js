5465d9f53032ce2c82122df939b539f3ac507fda
Bryan Chen <xlchen1291@gmail.com>
2016-09-22 18:21:31 +1200

    update

--- xlc/scripts.10m.js
+++ xlc/scripts.10m.js
@@ -16,44 +16,50 @@ let outputs = [
     text: 'Release Keyboard',
     bash: '/bin/bash',
     param1: '/Users/bryan/scripts/release_keyboard.sh',
-    terminal: false,
+    // terminal: false,
   },
   {
     text: 'Steal Keyboard',
     bash: '/bin/bash',
     param1: '/Users/bryan/scripts/steal_keyboard.sh',
-    terminal: false,
+    // terminal: false,
   },
-  bitbar.sep,
-  'Docker Containers'
 ]
 
-const dockerContainers = execSync(`${docker} ps -a --format "{{.Names}} ({{.Image}})|{{.ID}}|{{.Status}}"`)
-  .toString()
-  .trim()
-  .split('\n')
-  .map((line) => {
-    const [name, id, status] = line.split('|')
-    let color;
-    let cmd;
-    if (status.indexOf('Exited') !== -1) {
-      color = 'red'
-      cmd = 'start'
-    } else {
-      color = 'green'
-      cmd = 'stop'
-    }
-    return {
-      text: name,
-      refresh: true,
-      color,
-      bash: docker,
-      param1: cmd,
-      param2: id,
-      terminal: false
-    }
-  })
+try {
+  const dockerContainers = execSync(`${docker} ps -a --format "{{.Names}} ({{.Image}})|{{.ID}}|{{.Status}}"`)
+    .toString()
+    .trim()
+    .split('\n')
+    .map((line) => {
+      const [name, id, status] = line.split('|')
+      let color;
+      let cmd;
+      if (status.indexOf('Exited') !== -1) {
+        color = 'red'
+        cmd = 'start'
+      } else {
+        color = 'green'
+        cmd = 'stop'
+      }
+      return {
+        text: name,
+        refresh: true,
+        color,
+        bash: docker,
+        param1: cmd,
+        param2: id,
+        terminal: false
+      }
+    })
 
-outputs = outputs.concat(dockerContainers)
+    outputs = outputs.concat([
+      bitbar.sep,
+      'Docker Containers'
+    ])
+    outputs = outputs.concat(dockerContainers)
+} catch (e) {
+  
+}
 
 bitbar(outputs)
